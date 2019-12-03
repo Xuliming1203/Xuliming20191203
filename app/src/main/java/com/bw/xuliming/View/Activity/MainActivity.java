87a44b89611603e3a50f2477f4363660d17bd650
wowangla6 <Xuliming1203>
2019-12-03 08:43:43 +0800

    徐黎明第一周技能

---
+++ app/src/main/java/com/bw/xuliming/View/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bw.xuliming.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bw.xuliming.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/bw/xuliming/View/a.java
@@ -0,0 +1,9 @@
+package com.bw.xuliming.View;
+
+/**
+ * 时间：2019/12/3
+ * 作者：徐黎明
+ * 类的作用：
+ */
+public class a {
+}
